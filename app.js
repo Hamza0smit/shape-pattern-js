c957114893949e7d7c17df3547b935046c025900
Hamza0smit <hussainmubasher52@gmail.com>
2024-10-14 00:30:16 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,117 @@
+var userValue =prompt("Enter A first Number for triangal")
+
+for(i=1; i<=5 ; i++)
+{
+    for (j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1 ; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br>");
+
+
+// triangal pattren End
+
+
+
+// Reversed triangal pattaren start
+var userValue=prompt("Enter a second number")
+for(i=5; i>=1; i--)
+{
+    for(j=5;j>i; j--)
+        {
+            document.write(" &nbsp ");
+        }
+        for (k=1; k<=((2*i)-1);k++)
+        {
+            document.write("*");
+        }
+    document.write("<br>");
+}
+
+
+
+// Reversed triangal pattaren End
+
+
+
+// Diamond pattren is Start
+document.write("<br>")
+
+
+var userValue=prompt("Enter a third number")
+
+
+for(i=1; i<=7 ; i++)
+    {
+        for (j=7; j>i; j--)
+        {
+            document.write(" &nbsp; ");
+        }
+        for(k=1 ; k<=((2*i)-1); k++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    for(i=7; i>=1; i--)
+        {
+            for(j=7;j>i; j--)
+                {
+                    document.write(" &nbsp ");
+                }
+                for (k=1; k<=((2*i)-1);k++)
+                {
+                    document.write("*");
+                }
+            document.write("<br>");
+        }
+
+
+        document.write("<br>")
+
+        // / Diamond pattren is End
+
+
+
+        
+ var userValue=prompt("enter afour num")
+
+
+ for(i=5; i>=1; i--)
+    {
+        for(j=5;j>i; j--)
+            {
+                document.write(" &nbsp ");
+            }
+            for (k=1; k<=((2*i)-1);k++)
+            {
+                document.write("*");
+            }
+        document.write("<br>");
+    }
+
+    
+for(i=1; i<=5 ; i++)
+    {
+        for (j=5; j>i; j--)
+        {
+            document.write(" &nbsp; ");
+        }
+        for(k=1 ; k<=((2*i)-1); k++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+
+document.write("<br>")  
